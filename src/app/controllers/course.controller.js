b76248b7729a406a7d0bfd17b551caa8f89c8ecb
hoaian03IT <dangtranhoaian8603@gmail.com>
2024-10-29 23:53:31 +0700

    updates something

--- src/app/controllers/course.controller.js
+++ src/app/controllers/course.controller.js
@@ -1,8 +1,130 @@
 const { pool } = require("../../connectDB");
-const imageToBlob = require("../../utils/imageToBlob");
-const path = require("path");
 
 class Course {
+	async getCourseInformation(req, res) {
+		let conn;
+		try {
+			conn = await pool.getConnection();
+			const { "course-id": courseId } = req.query;
+			conn.query("CALL SP_GetCourseInformation(?)", [courseId])
+				.then(response => {
+					res.status(200).json(response[0][0][0]);
+				})
+				.catch(error => {
+					if (error.sqlState == 45000) {
+						res.status(404).json({ messageCode: "COURSE_NOT_FOUND" });
+					} else {
+						res.status(500).json({ message: error.message });
+					}
+				});
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+		} finally {
+			pool.releaseConnection(conn);
+		}
+	}
+
+	async getCourseContent(req, res) {
+		let conn;
+		try {
+			conn = await pool.getConnection();
+			const { "course-id": courseId } = req.query;
+			conn.query("CALL SP_GetCourseContent(?)", [courseId])
+				.then(response => {
+					let content = { courseId, levels: [] };
+					let array = response[0][0];
+					let levels = [];
+
+					let currentLevelId = array[0]["level id"];
+					let count = 0;
+					// chuyen cac record thanh dang array: {levelId: X, words: [{'word id': X, word: X, type: X, definition: X}]}
+					levels.push({ levelId: currentLevelId, levelName: array[0]["level name"], words: [] });
+					for (let item of array) {
+						if (item["level id"] !== currentLevelId) {
+							currentLevelId = item["level id"];
+							count++;
+							levels.push({ levelId: item["level id"], levelName: item["level name"], words: [] });
+						}
+						levels[count].words.push({
+							"word id": item["word id"],
+							word: item["word"],
+							type: item["type"],
+							definition: item["definition"],
+						});
+					}
+
+					content.levels = levels;
+					res.status(200).json(content);
+				})
+				.catch(error => {
+					if (error.sqlState == 45000) {
+						res.status(404).json({ messageCode: "COURSE_NOT_FOUND" });
+					} else {
+						res.status(500).json({ message: error.message });
+					}
+				});
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+		} finally {
+			pool.releaseConnection(conn);
+		}
+	}
+
+	async getCourseComment(req, res) {
+		let conn;
+		try {
+			conn = await pool.getConnection();
+			const { "course-id": courseId } = req.query;
+			conn.query("CALL SP_GetCourseComment(?)", [courseId])
+				.then(response => {
+					let comments = {};
+					let array = response[0][0];
+					// chia cac comment thanh 2 loai: feedback va replies
+					for (let item of array) {
+						if (item["reply comment id"] === null) {
+							comments[item["comment id"]] = {
+								commentId: item["comment id"],
+								comment: item["comment"],
+								firstName: item["first name"],
+								lastName: item["last name"],
+								username: item["username"],
+								avatarImage: item["avatar image"],
+								createdAt: item["created at"],
+								rate: item["rate"],
+								role: item["role name"],
+								replies: [],
+							};
+						} else {
+							comments[item["reply comment id"]].replies.push({
+								commentId: item["comment id"],
+								comment: item["comment"],
+								firstName: item["first name"],
+								lastName: item["last name"],
+								username: item["username"],
+								avatarImage: item["avatar image"],
+								createdAt: item["created at"],
+								rate: item["rate"],
+								role: item["role name"],
+							});
+						}
+
+					}
+					res.status(200).json(comments);
+				})
+				.catch(error => {
+					if (error.sqlState == 45000) {
+						res.status(404).json({ messageCode: "COURSE_NOT_FOUND" });
+					} else {
+						res.status(500).json({ message: error.message });
+					}
+				});
+		} catch (error) {
+			res.status(500).json({ message: error.message });
+		} finally {
+			pool.releaseConnection(conn);
+		}
+	}
+
 	async createCourseInformation(req, res) {
 		let conn;
 		try {
@@ -41,7 +163,6 @@ class Course {
 					res.status(500).json({ message: err.message });
 				});
 
-			pool.releaseConnection(conn);
 		} catch (error) {
 			res.status(500).json({ message: error.message });
 		} finally {
--- src/routes/course.route.js
+++ src/routes/course.route.js
@@ -4,11 +4,14 @@ const { authenticate } = require("../app/middlewares/authenticate.js");
 
 // middleware that is specific to this router
 const timeLog = (req, res, next) => {
-    console.log("Time: ", Date.now());
-    next();
+	console.log("Time: ", Date.now());
+	next();
 };
 router.use(timeLog);
 
+router.get("/overview", CourseController.getCourseInformation);
+router.get("/comment", CourseController.getCourseComment);
+router.get("/content", CourseController.getCourseContent);
 router.post("/create", [authenticate], CourseController.createCourseInformation);
 router.post("/update", [authenticate], CourseController.updateCourseInformation);
 router.delete("/destroy/:courseId", [authenticate], CourseController.destroyOwnCourse);
