5aa4504c769aad05fed51a78d734338b6a82318c
hoaian03IT <dangtranhoaian8603@gmail.com>
2024-12-06 00:52:41 +0700

    fix generateToken.js

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,13 +1,21 @@
 const jwt = require("jsonwebtoken");
 
 function generateAccessToken({ userId, email }) {
-	return jwt.sign({ "user id": userId, email: email }, process.env.ACCESS_TOKEN_SECRET, {
+	return jwt.sign({
+		"user id": userId,
+		email: email,
+		date: new Date().toISOString(),
+	}, process.env.ACCESS_TOKEN_SECRET, {
 		expiresIn: 180,
 	});
 }
 
 function generateRefreshToken({ userId, email, expiresIn }) {
-	return jwt.sign({ "user id": userId, email: email }, process.env.REFRESH_TOKEN_SECRET, {
+	return jwt.sign({
+		"user id": userId,
+		email: email,
+		date: new Date().toISOString(),
+	}, process.env.REFRESH_TOKEN_SECRET, {
 		expiresIn: expiresIn,
 	});
 }
